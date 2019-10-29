9a5f7a66f70b444946829e57496821cd64defb68
Sahil Iyer <iyersahil12@gmail.com>
2019-10-29 19:53:17 +1100

    Changed texture filenames for easier sorting.

--- src/entity.cpp
+++ src/entity.cpp
@@ -11,7 +11,7 @@ void ballReset(SDL_Renderer* renderer, Ball& ball)
 		ball.texture.texture = nullptr;
 	}
 
-	ball.texture = createTexture(renderer, "res/whiteball.png");
+	ball.texture = createTexture(renderer, "res/ball_white.png");
 	ball.texture.rect = SDL_Rect { (SCREEN_WIDTH - BALL_WIDTH) / 2, (SCREEN_HEIGHT  - BALL_HEIGHT) / 2, BALL_WIDTH, BALL_HEIGHT };
 	
 	ball.velocity.x = 0;
@@ -20,7 +20,7 @@ void ballReset(SDL_Renderer* renderer, Ball& ball)
 
 void paddleReset(SDL_Renderer* renderer, Paddle& paddle)
 {
-	paddle.texture = createTexture(renderer, "res/whitepaddle.png");
+	paddle.texture = createTexture(renderer, "res/paddle_white.png");
 	paddle.texture.rect = SDL_Rect { (SCREEN_WIDTH - PADDLE_WIDTH) / 2, (SCREEN_HEIGHT * 9 / 10) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT };
 	paddle.velocity.x = 0;
 	paddle.velocity.y = 0;
@@ -43,7 +43,7 @@ void bricksReset(GameData& gameData)
 
 	for (Brick& brick : gameData.bricks)
 	{
-		brick.texture = createTexture(gameData.renderer, "res/whitebrick.png");
+		brick.texture = createTexture(gameData.renderer, "res/brick_white.png");
 		brick.texture.rect = { brickX, brickY, BRICK_WIDTH, BRICK_HEIGHT };
 
 		brickX += BRICK_WIDTH + (SCREEN_WIDTH / 12);
