ccc7137d72a1c70f47b73ceb6906da30314121cc
Sahil Iyer <iyersahil12@gmail.com>
2019-11-16 11:54:30 +1100

    Added icon to breakout executable.

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ rem TODO: Switch between x86 and x64
 set depsDir=P:\Breakout\dependencies
 
 set compilerFlags=/nologo /Fe:breakout.exe /Fo:breakout.obj /MT /GR- /Gm- /EHsc /Od /Oi /FC /Zi /WX /W4 /wd4100 /IP:\Breakout\src /I%depsDir%\sdl2\include\
-set linkerFlags=/opt:ref /SUBSYSTEM:console /LIBPATH:%depsDir%\sdl2\lib\ sdl2.lib sdl2main.lib sdl2_image.lib sdl2_ttf.lib sdl2_mixer.lib
+set linkerFlags=/opt:ref /SUBSYSTEM:console /LIBPATH:%depsDir%\sdl2\lib\ sdl2.lib sdl2main.lib sdl2_image.lib sdl2_ttf.lib sdl2_mixer.lib resources.res
 
 set files=..\src\main.cpp
 
@@ -14,6 +14,9 @@ IF %ERRORLEVEL% NEQ 0 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\
 
 IF NOT EXIST build\ mkdir build
 pushd build
+
+rem NOTE(fkp): Windows only
+rc /nologo /fo resources.res ../resources.rc
 
 cl %compilerFlags% %files% /link %linkerFlags%
 IF %ERRORLEVEL% EQU 0 breakout.exe
